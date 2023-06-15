0023cec0dcb2b9374a14156b66867de88f83adf1
DELL <DELL@DESKTOP-4RLC0IP>
2023-06-15 17:59:25 -0500

    proyecto examen SA

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.intsuperior.vista;
 import ec.edu.intsuperior.controlador.Controlador;
 
-/**
- *
- * @author DELL
- */
 public class Aplicacion {
     public static void main(String[] args) {
         Controlador control= new Controlador();
